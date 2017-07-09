b3e6e1b713d4a095d6fa69d9dcebe3935f264675
goreleaserbot <goreleaser@carlosbecker.com>
2017-07-09 15:06:36 -0600

    malice version v0.3.10

--- Formula/malice.rb
+++ Formula/malice.rb
@@ -1,9 +1,9 @@
 class Malice < Formula
   desc ""
   homepage ""
-  url "https://github.com/maliceio/malice/releases/download/v0.3.9/malice_0.3.9_macOS_amd64.tar.gz"
-  version "0.3.9"
-  sha256 "41c5db6dd70488b66cf3f4beeffd8929e9e3d3682dc4bc9db491da7e4a6e9b8e"
+  url "https://github.com/maliceio/malice/releases/download/v0.3.10/malice_0.3.10_macOS_amd64.tar.gz"
+  version "0.3.10"
+  sha256 "3af0bf2895870d67428e5cb4c2416bdbec55f81e0dd48e05f3d775a7d6565384"
 
   def install
     bin.install "malice"
