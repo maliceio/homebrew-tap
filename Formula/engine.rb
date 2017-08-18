5cce94c703a8a18e35881395896213700d104bde
goreleaserbot <goreleaser@carlosbecker.com>
2017-08-18 17:47:32 -0600

    engine version 0.3.13

--- Formula/engine.rb
+++ Formula/engine.rb
@@ -1,9 +1,9 @@
 class Engine < Formula
   desc "Open Source Malware Analysis Framework."
   homepage "https://github.com/maliceio/engine"
-  url "https://github.com/maliceio/engine/releases/download/0.3.12/engine_0.3.12_macOS_amd64.tar.gz"
-  version "0.3.12"
-  sha256 "f50aeccad5a4a3516631ab21509553ba06c97bbc4d90659e4ea92bfc0450c12b"
+  url "https://github.com/maliceio/engine/releases/download/0.3.13/engine_0.3.13_macOS_amd64.tar.gz"
+  version "0.3.13"
+  sha256 "52c99078caaca73ce5d7afdad2b3fd3a227b94a9d9219622b15c6a14e272be4b"
 
   def install
     bin.install "engine"
