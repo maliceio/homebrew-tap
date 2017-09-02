4e6aab42e838e02d6f8c7f9500fd524e9bdf894b
goreleaserbot <goreleaser@carlosbecker.com>
2017-09-01 23:20:39 -0600

    engine version 0.3.16

--- Formula/engine.rb
+++ Formula/engine.rb
@@ -3,7 +3,7 @@ class Engine < Formula
   homepage "https://github.com/maliceio/engine"
   url "https://github.com/maliceio/engine/releases/download/0.3.16/engine_0.3.16_macOS_amd64.tar.gz"
   version "0.3.16"
-  sha256 "acbfa8168db2975fae520a8818bbcd54af6b07ad98a1f7124b2867efe8cf6819"
+  sha256 "52032756aaea2b1a458cad09b89a5b5de293d290186a200d020519af92528ee2"
 
   def install
     bin.install "engine"
