8de88cea3f7268712be8506da743aa2dce2dc127
goreleaserbot <goreleaser@carlosbecker.com>
2017-09-06 21:08:02 -0600

    cli version 17.09.0-dev

--- Formula/cli.rb
+++ Formula/cli.rb
@@ -3,7 +3,7 @@ class Cli < Formula
   homepage "https://github.com/maliceio/malice"
   url "https://github.com/maliceio/cli/releases/download/17.09.0-dev/cli_17.09.0-dev_macOS_amd64.tar.gz"
   version "17.09.0-dev"
-  sha256 "37e6f5080322a4e33c9125b2ec23a7c27108da401d65f127f5df952925ba5ad4"
+  sha256 "ceb7899148e85218f4bbe7ce699e57ea9c5ec3bce8bb8b86e7e92006192ccc00"
 
   def install
     bin.install "malice"
