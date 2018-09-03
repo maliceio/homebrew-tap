bc3914a4368ec5c55446b767c85352b43e8e5be7
goreleaserbot <goreleaser@carlosbecker.com>
2018-09-03 16:25:19 -0400

    Brew formula update for malice version v0.3.20

--- Formula/malice.rb
+++ Formula/malice.rb
@@ -3,7 +3,7 @@ class Malice < Formula
   homepage "https://github.com/maliceio/malice"
   url "https://github.com/maliceio/malice/releases/download/v0.3.20/malice_0.3.20_macOS_amd64.tar.gz"
   version "0.3.20"
-  sha256 "5d1194dfe1cc53b8802800465bcb6dd40310405f36dc12ded24e9d8ce536b6fd"
+  sha256 "b014b925d4a28505c20d1e9766144cb2bbfbf48711941e655696bc9af243e4c4"
 
   def install
     bin.install "malice"
