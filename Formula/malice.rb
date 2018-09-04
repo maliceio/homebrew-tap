66b11cac9297c53bfc2e52808219d084c887bbd9
goreleaserbot <goreleaser@carlosbecker.com>
2018-09-03 23:21:38 -0400

    Brew formula update for malice version v0.3.21

--- Formula/malice.rb
+++ Formula/malice.rb
@@ -1,9 +1,9 @@
 class Malice < Formula
   desc "Open Source Malware Analysis Framework."
   homepage "https://github.com/maliceio/malice"
-  url "https://github.com/maliceio/malice/releases/download/v0.3.20/malice_0.3.20_macOS_amd64.tar.gz"
-  version "0.3.20"
-  sha256 "b014b925d4a28505c20d1e9766144cb2bbfbf48711941e655696bc9af243e4c4"
+  url "https://github.com/maliceio/malice/releases/download/v0.3.21/malice_0.3.21_macOS_amd64.tar.gz"
+  version "0.3.21"
+  sha256 "fdd16e35a06c97a0a08ce08da7b0c303e0268566eecf57af52d6406bb560dde5"
 
   def install
     bin.install "malice"
